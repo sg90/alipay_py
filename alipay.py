0ea505c762dc2d805a5d3f9d96a2c16648c397ad
Shangguan Jianfeng <jfsg1990@gmail.com>
2016-04-10 19:19:51 +0800

    modify make_payment_url

--- alipay.py
+++ alipay.py
@@ -1,43 +1,55 @@
 #!/usr/bin/env python
 #coding:utf-8
-from tornado import gen
-from tornado import web
+import tornado.gen
+import tornado.web
+import tornado.httpserver
+
 import alipay_config
 import alipay_core
-from alipay_core import *
 
 
-host = "http://baidu.com" # 这里改为你自己的host
-class MakePaymentInfo(web.RequestHandler):
+def make_payment_url(out_trade_no, subject, total_fee, service_type, body=None, show_url=None, timeout=None):
     """
-    构造一个支付请求
+        构造支付请求参数
     """
-    def make_payment_info(self, out_trade_no=None, subject=None, total_fee=None, body=None):
+    order_info = {'partner': '%s' % (alipay_config.partner_id),
+                  'service': '%s' % service_type,
+                  '_input_charset': 'utf-8',
+                  'notify_url': '%s' % (alipay_config.notify_url),
+                  'return_url': '%s' % (alipay_config.return_url),
+                  #业务参数
+                  'out_trade_no': '%s' % (out_trade_no),
+                  'subject': '%s' % subject,
+                  'seller_id': alipay_config.partner_id,
+                  'payment_type': '1',
+    }
 
-        order_info = {"partner": "%s" % (alipay_config.partner_id),
-                      "service": "mobile.securitypay.pay",
-                      "_input_charset": "utf-8",
-                      "notify_url": "http://%s/callback" % (host),
-                      #业务参数
-                      "out_trade_no": None,
-                      "paymnet_type": "1",
-                      "subject": None,
-                      "seller_id": alipay_config.alipay_account,
-                      "total_fee": 0,
-                      "body": None
-        }
+    if not total_fee or total_fee <= 0.0:
+        total_fee = 0.01
+    order_info['total_fee'] = total_fee
 
-        order_info["out_trade_no"] = "%s" % (out_trade_no)
-        order_info["subject"] = "%s" % (subject)
-        if total_fee <= 0.0:
-            total_fee = 0.01
-        order_info["total_fee"] = total_fee
-        order_info["body"] = "hsh_shop"
-        return order_info
+    if not show_url:
+        show_url = alipay_config.show_url
+
+    order_info['show_url'] = show_url
+    if body:
+        order_info['body'] = body
+    
+    if timeout:
+        order_info['it_b_pay'] = timeout
+
+    # make request url
+    url = alipay_core.make_payment_request(order_info)
+    return url
 
 
-    @gen.coroutine
-    def get(self, order_id):
+class MakePaymentInfo(tornado.web.RequestHandler):
+    """
+    构造一个支付请求URL
+    """
+
+    @tornado.gen.coroutine
+    def get(self, service_type, order_id):
         """
         这里是客户端请求服务端来获取提交给支付宝的支付请求， 因为私钥放在客户端来签名的话，可能会遭遇破解，所以构造了这个服务端用来签名的方法
         """
@@ -51,91 +63,87 @@ class MakePaymentInfo(web.RequestHandler):
         STATUS_ORDER_REFUND = 3 #订单已退款
         STATUS_ORDER_STATUS_ERROR = 4  #订单状态错误
 
+        total_fee = 0.01  #这里将金额设为1分钱，方便测试
+        body = 'test'
+        if service_type == 'web':
+            service_type = alipay_config.web_pay_service
+        elif service_type == 'wap':
+            service_type = alipay_config.wap_pay_service
+
+        res = make_payment_url(out_trade_no=order_id, subject=order_id, total_fee=total_fee, service_type=service_type, body=body, show_url='http://www.baidu.com')
+        self.write({'status': STATUS_SUCCESS, 'res': res})
 
 
-
-        total_fee = 0.01  #这里讲金额设为1分钱，方便测试
-        body = ""
-        payment_info = self.make_payment_info(out_trade_no=order_id, subject=order_id, total_fee=total_fee,
-                                              body=body)
-        res = alipay_core.make_payment_request(payment_info)
-        self.write({"status": STATUS_SUCCESS, "res": res})
-
-
-class PaymentCallBack(web.RequestHandler):
+class PaymentCallback(tornado.web.RequestHandler):
     """
     阿里支付回调
     """
-    """
-    WAIT_BUY
-    body=商品描述&buyer_email=ma.hongwei@foxmail.com&buyer_id=2088702056383644&discount=0.00&gmt_create=2015-07-10 18:06:54&is_total_fee_adjust=Y&notify_id=cf579a7403c6bdc806ccb49101510aa05k&notify_time=2015-07-10 18:06:54&notify_type=trade_status_sync&out_trade_no=PRH0BL7A6QLCSUO&payment_type=1&price=0.01&quantity=1&seller_email=xiaowenwen@7500.com.cn&seller_id=2088021072549071&sign=Lp3JJj4WpR1BggPiC/O0IPGGeIc5xxPLhA7ICvEYElaEv820Ju8GeFXKjepvIlgWa76lMMGW99BPW0HkmqvPRE1thJevKNHQ+KQTIXpw5iKaoXbAXeURrs4LUK6CTIQBpX0fAjfZ4EMAHnx31fgRnJbuuWCOTJ8MLgjLvDmJs9M=&sign_type=RSA&subject=商品测试&total_fee=0.01&trade_no=2015071000001000640055508270&trade_status=WAIT_BUYER_PAY&use_coupon=N
 
-    PAYSUCCESS
-    body=商品描述&buyer_email=ma.hongwei@foxmail.com&buyer_id=2088702056383644&discount=0.00&gmt_create=2015-07-10 18:06:54&gmt_payment=2015-07-10 18:06:55&is_total_fee_adjust=N&notify_id=f00d4988288e8e4af8bff37f4b0e365b5k&notify_time=2015-07-10 18:06:55&notify_type=trade_status_sync&out_trade_no=PRH0BL7A6QLCSUO&payment_type=1&price=0.01&quantity=1&seller_email=xiaowenwen@7500.com.cn&seller_id=2088021072549071&sign=CfTvBdJ6blsg0BjDLq51eJn073VkVkoK79kTFOURuN4Av48lvJxN4S4lqm+sRW6xA7AlKBm/pNKqz0eRT1jzFvaG8Nd4C2vMKYzzQ/CrTkDym74od+q04Sdp2X+xB5fKfqeOy3qyFJgpXX1WT7npXjWqAxmT80ZTQiykoufWnNo=&sign_type=RSA&subject=商品测试&total_fee=0.01&trade_no=2015071000001000640055508270&trade_status=TRADE_SUCCESS&use_coupon=N
-
-    """
+    @tornado.gen.coroutine
+    def get(self):
+        yield self.post()
 
     #这里需要做3件事情
     #1验证平台签名
     #2去阿里验证回调信息是否真实
     #3执行业务逻辑，回调订单状态
-
-    @gen.coroutine
+    @tornado.gen.coroutine
     def post(self):
-        args = self.request.arguments
-        for k, v in args.items():
-            args[k] = v[0]
+        params = self.request.arguments
+        for k, v in params.items():
+            params[k] = v[0]
 
-        check_sign = params_to_query(args)
-        params = query_to_dict(check_sign)
-        sign = params['sign']
-        params = params_filter(params)
-        message = params_to_query(params,quotes=False,reverse=False) #获取到要验证签名的串
-        check_res = check_ali_sign(message,sign)  #验签
-
+        check_res = alipay_core.verify_alipay_request_sign(params)  #验签
+        
         if check_res == False:
-            self.write("fail")
+            self.write('fail')
             return
 
         #这里是去访问支付宝来验证订单是否正常
-        res = verify_from_gateway({"partner": alipay_config.partner_id, "notify_id": params["notify_id"]})
+        res = alipay_core.verify_from_gateway(params['notify_id'])
+        
         if res == False:
-            self.write("fail")
+            self.write('fail')
             return
 
-        trade_status = params["trade_status"]
-        order_id = params["out_trade_no"]  #你自己构建订单时候的订单ID
-        alipay_order = params["trade_no"]  #支付宝的订单号码
-        total_fee = params["total_fee"]  #支付总额
+        if 'trade_status' not in params:
+            return
+        trade_status = params['trade_status']
+        order_id = params['out_trade_no']  #你自己构建订单时候的订单ID
+        alipay_order = params['trade_no']  #支付宝的订单号码
+        total_fee = params['total_fee']  #支付总额
 
         """
         下面是处理付款完成的逻辑
         """
-        if trade_status == "TRADE_SUCCESS":  #交易成功
+        print trade_status
+        if trade_status == 'TRADE_SUCCESS':  #交易成功
+            pass
             #TODO:这里来做订单付款后的操作
-            self.write("success")
-            return
-        if trade_status == "TRADE_FINISHED":
-            return
-
-        if trade_status == "WAIT_BUYER_PAY":
-
-            self.write("success")
-            return
-        if trade_status == "TRADE_CLOSED":#退款会回调这里
-            self.write("success")
-
+        elif trade_status == 'TRADE_FINISHED':
+            pass
+        elif trade_status == 'WAIT_BUYER_PAY': # trade created, wait user to pay
+            pass
+        elif trade_status == 'TRADE_PENDING': # 等待卖家收款（买家付款后，如果卖家账号被冻结）
+            print 'WARNING: READE_PENDING'
+        elif trade_status == 'TRADE_CLOSED': # 在指定时间段内未支付时关闭的交易,在交易完成全额退款成功时关闭的交易
+            pass
+        
+        self.write('success')
 
 
+def main():
+    application = tornado.web.Application([
+        (r'/alipay/make_payment_url/(\w*)/(\w*)', MakePaymentInfo),
+        (r'/alipay/payment_callback', PaymentCallback),
+    ])
+    settings = {
+        "debug": True,
+        'autoreload': True
+    }
+    httpServer = tornado.httpserver.HTTPServer(application, settings)
+    httpServer.listen(9998)
+    tornado.ioloop.IOLoop.instance().start()
 
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    main()
--- alipay_core.py
+++ alipay_core.py
@@ -1,29 +1,22 @@
 #coding:utf-8
-__author__ = 'zhanglong'
+__author__ = 'sg90'
 
 import rsa
 import alipay_config
 import base64
 
-SIGN_TYPE = "SHA-1"
+SIGN_TYPE = 'SHA-1'
 import urllib
 import requests
 import hashlib
 
 
 def params_filter(params):
-    """
-    去掉不需要验证前面的参数
-    :param params:
-    :return:
-    """
-    """
-    :param params:
-    :return:
-    """
     ret = {}
+    if not params:
+        return ret
     for key, value in params.items():
-        if key == "sign" or key == "sign_type" or value == "":
+        if key == 'sign' or key == 'sign_type' or value == '':
             continue
         ret[key] = value
     return ret
@@ -36,9 +29,9 @@ def query_to_dict(query):
     :return:
     """
     res = {}
-    k_v_pairs = query.split("&")
+    k_v_pairs = query.split('&')
     for item in k_v_pairs:
-        sp_item = item.split("=", 1)  #注意这里，因为sign秘钥里面肯那个包含'='符号，所以splint一次就可以了
+        sp_item = item.split('=', 1)  #注意这里，因为sign秘钥里面可能那个包含'='符号，所以split一次就可以了
         key = sp_item[0]
         value = sp_item[1]
         res[key] = value
@@ -47,22 +40,13 @@ def query_to_dict(query):
 
 
 def params_to_query(params, quotes=False, reverse=False):
-    """
-        生成需要签名的字符串
-    :param params:
-    :return:
-    """
-    """
-    :param params:
-    :return:
-    """
-    query = ""
+    query = ''
     for key in sorted(params.keys(), reverse=reverse):
         value = params[key]
         if quotes == True:
-            query += str(key) + "=\"" + str(value) + "\"&"
+            query += str(key) + '=\"' + str(value) + '\"&'
         else:
-            query += str(key) + "=" + str(value) + "&"
+            query += str(key) + '=' + str(value) + '&'
     query = query[0:-1]
     return query
 
@@ -70,8 +54,6 @@ def params_to_query(params, quotes=False, reverse=False):
 def make_sign(message):
     """
     签名
-    :param message:
-    :return:
     """
     private_key = rsa.PrivateKey._load_pkcs1_pem(alipay_config.RSA_PRIVATE)
     sign = rsa.sign(message, private_key, SIGN_TYPE)
@@ -117,17 +99,14 @@ def check_ali_sign(message, sign):
 
 def make_payment_request(params_dict):
     """
-    构造一个支付请求的信息，包含最终结果里面包含签名
-    :param params_dict:
-    :return:
+    构造一个支付请求URL，包含最终签名
     """
-    query_str = params_to_query(params_dict, quotes=True) #拼接签名字符串
+    query_str = params_to_query(params_dict) #拼接签名字符串
     sign = make_sign(query_str) #生成签名
     sign = urllib.quote_plus(sign)
-    res = "%s&sign=\"%s\"&sign_type=\"RSA\"" % (query_str, sign)
+    res = '%s&sign=%s&sign_type=RSA' % (query_str, sign)
+    res = alipay_config.gateway + res
     return res
-
-
 
 
 def verify_alipay_request_sign(params_dict):
@@ -137,85 +116,30 @@ def verify_alipay_request_sign(params_dict):
     :return:True or False
     """
     sign = params_dict['sign']
+    missingPadding = 4 - len(sign) % 4
+    if missingPadding:
+        sign += '=' * missingPadding
+        print sign
     params = params_filter(params_dict)
     message = params_to_query(params, quotes=False, reverse=False)
     check_res = check_ali_sign(message, sign)
     return check_res
 
 
-def verify_from_gateway(params_dict):
+def verify_from_gateway(notify_id):
     """
-    从阿里网管验证请求是否正确
+    从阿里网关验证请求是否正确
     :param params_dict:
     :return:
     """
-    ali_gateway_url = "https://mapi.alipay.com/gateway.do?service=notify_verify&partner=%(partner)d&notify_id=%(notify_id)s"
-    notify_id = params_dict["notify_id"]
-    partner = alipay_config.partner_id
-    ali_gateway_url = ali_gateway_url % {"partner": partner, "notify_id": notify_id}
+    ali_gateway_url = alipay_config.gateway + 'service=' + alipay_config.notify_verify_service + '&partner=' + alipay_config.partner_id + '&notify_id=' + notify_id
+
     res = requests.get(ali_gateway_url)
-    #    res_dict = encoder.XML2Dict.parse(res.text)
-    if res.text == "true":
+    if res.text == 'true':
         return True
+    
     return False
 
 
-#test
-def test():
-    params = {"a": 1, "b": 2, "c": "", 1: 1, "sign": "asdfasdfas", "sign_type": "rsa"}
-    after_params = params_filter(params)
-    assert after_params == {"a": 1, "b": 2, 1: 1}
-    query = params_to_query(after_params)
-
-    assert query == '1=1&a=1&b=2'
-    print query
-
-    query2 = params_to_query(after_params, quotes=True)
-    assert query2 == '1="1"&a="1"&b="2"'
-    print query2
-
-    sign = make_sign(query)
-    #print sign
-    sign_res = check_sign(query, sign)
-    assert sign_res == True
-
-    check_signa = "body=商品描述&buyer_email=zhanglwork@gmail.com&buyer_id=2088102716951071&discount=0.00&gmt_create=2015-07-13 10:28:00&gmt_payment=2015-07-13 10:28:01&is_total_fee_adjust=N&notify_id=83ee2b993b46d3f5d1d27b9078199d062e&notify_time=2015-07-13 10:28:01&notify_type=trade_status_sync&out_trade_no=1WZ6ZYT9VYCTLN6&payment_type=1&price=0.01&quantity=1&seller_email=xiaowenwen@7500.com.cn&seller_id=2088021072549071&sign=H5VGZ63LYr3f9819ABuBuaxzRVOx5u3Ku3BI661jkW5gisD1XMc4PdV6bfI/5EIEFQvmSKLADYG3I/8N8Ty5eu/xsrcQXjsVC3Zr3wLOXaDnYh8Ale2crDoIQjgUrbg4d8csovBrJV9Fi+/SCM2/EXPxlO0qrilY/EpKYOczzZ8=&sign_type=RSA&subject=商品测试&total_fee=0.01&trade_no=2015071300001000070073886063&trade_status=TRADE_SUCCESS&use_coupon=N"
-
-    """
-  payment_url  body=商品描述&buyer_email=zhanglwork@gmail.com&buyer_id=2088102716951071&discount=0.00&gmt_create=2015-07-13 17:10:23&is_total_fee_adjust=Y&notify_id=a4b691fca6f6f79e816fe022e398d2532e&notify_time=2015-07-13 17:10:23&notify_type=trade_status_sync&out_trade_no=OWYNN72PRTU2G81&payment_type=1&price=0.10&quantity=1&seller_email=xiaowenwen@7500.com.cn&seller_id=2088021072549071&sign=oX0I0LR7YGH96d3fZY9MfLj7BlAWclwVR3kK5XMgmoWcjFTpclB6tXssL81a+JOsKP0bcPsbH3dygMjjCVZHnHOpArs0tzLutjj00XnqH8uXEAItTPs2Hf/ld3TIZqsdXYBfVHtZaiPko/CgN8VQwjjITW1IRIY5JTE/MWidE8A=&sign_type=RSA&subject=商品测试&total_fee=0.10&trade_no=2015071300001000070073922535&trade_status=WAIT_BUYER_PAY&use_coupon=N
-[I 150713 17:10:23 web:1728] 200 POST /consumer/api/v1/alipay_callback (127.0.0.1) 1.21ms
-payment_url  body=商品描述&buyer_email=zhanglwork@gmail.com&buyer_id=2088102716951071&discount=0.00&gmt_create=2015-07-13 17:10:23&gmt_payment=2015-07-13 17:10:24&is_total_fee_adjust=N&notify_id=4c8d6808aac61d3ead5cd1bb187374e72e&notify_time=2015-07-13 17:10:24&notify_type=trade_status_sync&out_trade_no=OWYNN72PRTU2G81&payment_type=1&price=0.10&quantity=1&seller_email=xiaowenwen@7500.com.cn&seller_id=2088021072549071&sign=KRffFX0OvuxQpusduxvMaJ0f6sVmY8Ta8go969W+sKkypkt8SoUTXpJ5jjysa+/y8CTazBd+K+1Co9my/RswoDBjjspupLiuU0QcrNDDIBPPathk6tEhv8/16CF+IFbn1HoPKVjeHheuBLzCyiEdveqN7ORk/2T/Q9KG0Qqqs/I=&sign_type=RSA&subject=商品测试&total_fee=0.10&trade_no=2015071300001000070073922535&trade_status=TRADE_SUCCESS&use_coupon=N
-
-    """
-
-    check_signa = "body=hsh_shop&buyer_email=ma.hongwei@foxmail.com&buyer_id=2088702056383644&discount=0.00&gmt_create=2015-07-28 19:56:11&is_total_fee_adjust=Y&notify_id=1f124ba6791b3fadfe0734e9345b00b75k&notify_time=2015-07-28 19:56:11&notify_type=trade_status_sync&out_trade_no=1438084559&payment_type=1&price=0.01&quantity=1&seller_email=xiaowenwen@7500.com.cn&seller_id=2088021072549071&sign=WhHQeslDI0YtWdPmXIvvsB9KBZza5Mrzy1AldAq3f6cNP4ebMdMhoXLHAzu9oujm4UxOmTZX60/suYc7ciqea5LCsJR55yUlf4mxrHYqMkYr9+Xt2r/nKaEDe3AXEQHFl+KHYrNiPm35WCmz7rsiTv5p0X3SWK5YhEWT9ycYoHU=&sign_type=RSA&subject=corp_order&total_fee=0.01&trade_no=2015072800001000640056773075&trade_status=WAIT_BUYER_PAY&use_coupon=N"
-
-    check_signa = "body=hsh_shop&buyer_email=ma.hongwei@foxmail.com&buyer_id=2088702056383644&discount=0.00&gmt_create=2015-07-28 19:56:11&gmt_payment=2015-07-28 19:56:11&is_total_fee_adjust=N&notify_id=f6124d74bab89c36a7b3be29285110d25k&notify_time=2015-07-28 19:56:11&notify_type=trade_status_sync&out_trade_no=1438084559&payment_type=1&price=0.01&quantity=1&seller_email=xiaowenwen@7500.com.cn&seller_id=2088021072549071&sign=NFQvSMX3NPxRarKeQ4uJYfnL0z4Kx/bMvI6GbWJUNXRrEWHJ+PnLqiOCvy1EuO+doVBxiwL8acHOFSxyXBnevVG+2cq10YMTvTVet1ouhQNrL6WDZpqKC6TSjq8SRDvQooi9Kjeee4PuFJ6rnkFhRIeFebshLVi7MX3E3x+f808=&sign_type=RSA&subject=corp_order&total_fee=0.01&trade_no=2015072800001000640056773075&trade_status=TRADE_SUCCESS&use_coupon=N"
-
-    params = query_to_dict(check_signa)
-    sign = params['sign']
-    #sign = sign.decode('utf-8')
-    params = params_filter(params)
-    message = params_to_query(params, quotes=False, reverse=False)
-    check_res = check_ali_sign(message, sign)
-    assert check_res == True
-    res = verify_from_gateway({"partner": alipay_config.partner_id, "notify_id": params["notify_id"]})
-    assert res == False
-
-
-def test_refund():
-    trade_order = "2015073000001000860056086838"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    pass
